184f7859893fb096aad31612023757f9e1a4b722
Karol Gracz <graczu42@gmail.com>
2017-06-11 12:19:04 +0200

    fixed logout button

--- animal_shelter/home_page/urls.py
+++ animal_shelter/home_page/urls.py
@@ -6,6 +6,6 @@ app_name = 'home_page'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login_user, name='login_user'),
-    url(r'^logout/$', views.login_user, name='logout_user'),
+    url(r'^logout/$', views.logout_user, name='logout_user'),
     url(r'^register/$', views.register, name='register')
 ]
