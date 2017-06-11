a21ffa9def1ee8a7ca2035a09b94257634c51e44
Karol Gracz <graczu42@gmail.com>
2017-06-11 12:27:03 +0200

    When user is logged access to / login and /registered is prevented. When user is not logged access to /logout is prevented

--- animal_shelter/home_page/views.py
+++ animal_shelter/home_page/views.py
@@ -4,11 +4,12 @@ from .form import RegistrationForm
 
 
 def index(request):
-    logged_in = False
-    return render(request, 'home_page/index.html', {'logged_in': logged_in})
+    return render(request, 'home_page/index.html')
 
 
 def login_user(request):
+    if request.user.is_authenticated():
+        return render(request, 'home_page/index.html')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,8 +18,7 @@ def login_user(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                logged_in = True
-                return render(request, 'home_page/index.html', {'logged_in': logged_in})
+                return render(request, 'home_page/index.html')
             else:
                 return render(request, 'home_page/login.html', {'error_message': 'Your account has been disabled'})
         else:
@@ -27,12 +27,15 @@ def login_user(request):
 
 
 def logout_user(request):
+    if not request.user.is_authenticated():
+        return render(request, 'home_page/index.html')
     logout(request)
-    logged_in = False
-    return render(request, 'home_page/index.html', {'logged_in': logged_in})
+    return render(request, 'home_page/index.html')
 
 
 def register(request):
+    if request.user.is_authenticated():
+        return render(request, 'home_page/index.html')
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -41,8 +44,7 @@ def register(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
-            logged_in = True
-            return render(request, 'home_page/index.html', {'logged_in': logged_in})
+            return render(request, 'home_page/index.html')
     else:
         form = RegistrationForm()
     return render(request, 'home_page/register.html', {'form': form})
