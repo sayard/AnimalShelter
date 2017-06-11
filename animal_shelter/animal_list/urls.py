d3ab07610807c6ddb035b86477f8cc4df02433a5
Karol Gracz <graczu42@gmail.com>
2017-06-11 20:27:09 +0200

    users can now mark pets for adoption

--- animal_shelter/animal_list/urls.py
+++ animal_shelter/animal_list/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import ListView, DetailView
 from .models import Animal
+from . import views
 
+app_name = 'animal_list'
 
 urlpatterns = [
     url(r'^$', ListView.as_view(
@@ -11,5 +13,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)$', DetailView.as_view(
         model=Animal,
         template_name="animal_list/details.html"
-    ))
+    )),
+    url(r'^adopt/(\d+)/$', views.adopt, name='adopt')
 ]
--- animal_shelter/animal_list/views.py
+++ animal_shelter/animal_list/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Animal
 
-# Create your views here.
+
+def adopt(request, animal_id):
+    animal = Animal.objects.get(pk=animal_id)
+    animal.is_visible = False
+    animal.save()
+    return render(request, 'home_page/index.html')
