e56e4aa93676e4379ced0337a3c28ef6540e312d
Karol Gracz <graczu42@gmail.com>
2017-06-11 20:54:09 +0200

    added adopt page

--- animal_shelter/animal_list/views.py
+++ animal_shelter/animal_list/views.py
@@ -6,4 +6,4 @@ def adopt(request, animal_id):
     animal = Animal.objects.get(pk=animal_id)
     animal.is_visible = False
     animal.save()
-    return render(request, 'home_page/index.html')
+    return render(request, 'home_page/adopt.html', {'animal': animal})
